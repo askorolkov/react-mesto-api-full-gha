5f145a4478b9f1d79aa6454eb03c5d755a4432b0
Aleksey Korolkov <askorolkov1@gmail.com>
2023-05-30 15:07:17 +0500

    cors options test

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,12 @@ const allowedCors = [
 ];
 
 const app = express();
-app.use(cors());
+app.use(cors(allowedCors));
 app.use((req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  console.log(origin);
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
@@ -34,6 +35,7 @@ app.use((req, res, next) => {
   }
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    console.log('includes');
   }
 
   return next();
