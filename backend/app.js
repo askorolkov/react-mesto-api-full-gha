27c90d6f80bc41eb1c2692bb3afc58c42c9926f5
Aleksey Korolkov <askorolkov1@gmail.com>
2023-05-30 17:44:28 +0500

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('./middlewares/cors');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
 
@@ -14,43 +14,9 @@ const { login, createUser } = require('./controllers/users');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
-const allowedCors = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'https://askorolkov.nomoredomains.rocks',
-  'http://askorolkov.nomoredomains.rocks',
-  'https://api.askorolkov.nomoredomains.rocks',
-  'https://api.askorolkov.nomoredomains.rocks',
-];
 
-app.use((req, res, next) => {
-  // Сохраняем источник запроса в переменную origin
-  const { origin } = req.headers;
-  // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-
-  return next();
-});
 app.use(requestLogger);
 app.post('/signin', onUserLoginValidation, login);
 app.post('/signup', onUserCreateValidation, createUser);
