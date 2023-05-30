a5cb24146aea7bcf680cbc80b4def042b5402edc
Aleksey Korolkov <askorolkov1@gmail.com>
2023-05-30 15:04:54 +0500

    cors options test

--- backend/app.js
+++ backend/app.js
@@ -12,22 +12,31 @@ const { onUserCreateValidation, onUserLoginValidation } = require('./middlewares
 const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
-const allowedCors = {
-  origin: [
-    'https://askorolkov.nomoredomains.rocks',
-    'http://askorolkov.nomoredomains.rocks',
-  ],
-  optionsSuccessStatus: 200,
-};
+const allowedCors = [
+  'https://askorolkov.nomoredomains.rocks',
+  'http://askorolkov.nomoredomains.rocks',
+];
+
 const app = express();
-app.use(cors(allowedCors));
+app.use(cors());
 app.use((req, res, next) => {
+  const { method } = req;
   const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  next();
+  return next();
 });
 app.use(bodyParser.json());
 app.use(requestLogger);
