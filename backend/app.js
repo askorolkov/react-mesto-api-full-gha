2c6768c6603ab81221cf127a608cc73e99184805
Aleksey Korolkov <askorolkov1@gmail.com>
2023-05-30 16:27:58 +0500

    cors test

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://askorolkov.nomoredomains.rocks',
-}));
+app.use(cors);
 
 app.use(bodyParser.json());
 app.use(requestLogger);
