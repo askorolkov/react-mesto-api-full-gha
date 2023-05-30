698aee609149b4227f371c8c25573b9e0144021e
Aleksey Korolkov <askorolkov1@gmail.com>
2023-05-30 14:30:45 +0500

    add cors options

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,23 @@ const { onUserCreateValidation, onUserLoginValidation } = require('./middlewares
 const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
-
+const allowedCors = {
+  origin: [
+    'https://askorolkov.nomoredomains.rocks/sign-in',
+    'http://askorolkov.nomoredomains.rocks/sign-in',
+  ],
+  optionsSuccessStatus: 200,
+};
 const app = express();
-app.use(cors());
+app.use(cors(allowedCors));
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+
+//   next();
+// });
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.post('/signin', onUserLoginValidation, login);
